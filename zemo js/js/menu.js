144832ff9462cb62d38845592fba3fe30a1012f9
Exayitoff <104882895+Exayitoff@users.noreply.github.com>
2022-06-22 11:18:15 +0500

    Add files via upload

---
+++ zemo js/js/menu.js
@@ -0,0 +1,25 @@
+
+
+let mX =   document.getElementById("naigatsiya");
+
+function menuBtn(){
+  mX.innerHTML = `<ul class="ul-nav">
+  <li><a href="index.html">Bosh Sahifa</a></li>
+  <li><a href="#">Haqida</a></li>
+  <li><a href="#">Xizmatlar</a></li>
+  <li><a href="#">Portfolio</a></li>
+  <li><a href="#">Aloqa</a></li>
+  </ul>`
+}
+
+
+
+
+
+
+
+
+
+
+
+
